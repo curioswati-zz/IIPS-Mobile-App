eacc3325552da98592d11eb15e93a6e93c0cc7d6
swati-jaiswal <jaiswalswati94@gmail.com>
2015-07-17 23:40:24 +0530

    adds state for edit profile view

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -80,7 +80,19 @@ angular.module('starter', ['ionic', 'starter.controllers', 'ngCordova'])
       url: "/profile",
       views: {
         'tab-profile': {
-          templateUrl: "templates/tab-profile.html"        }
+          templateUrl: "templates/tab-profile.html",
+          controller: 'ProCtrl'
+        }
+      }
+    })
+
+    .state('tab.edit-profile', {
+      url: "/edit-profile",
+      views: {
+        'tab-profile': {
+          templateUrl: "templates/edit-profile.html",
+          controller: 'ProCtrl'
+        }
       }
     });
 
